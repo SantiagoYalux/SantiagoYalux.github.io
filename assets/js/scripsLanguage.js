a8f0bf24730915882e544b107d6320cc1f0c66e6
Santiago Yalux <santiyalux@hotmail.com>
2023-02-26 01:21:18 -0300

    Multi idioma

--- assets/js/scripsLanguage.js
+++ assets/js/scripsLanguage.js
@@ -5,10 +5,19 @@ flagsElements.addEventListener("click", (e)=>{
     changeLanguage(e.target.parentElement.dataset.language)
 });
 
+const textsToChange = document.querySelectorAll("[data-section]")
+
 const changeLanguage = async language=>{
     const requestJson = await fetch(`./Languages/${language}.json`);
     
     const texts = await requestJson.json();
 
-    console.log(texts);
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        
+        textToChange.innerHTML = texts[section][value];
+    }
 }
+
+ 
